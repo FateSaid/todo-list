2a73a74644d1b3e05d7b65da28e528d3051c00f3
Fatesaid <fatesaid56@gmail.com>
2024-05-26 19:34:31 +0200

    include for in loop to display object values

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import './style.css';
-import { functionOne } from './todo.js';
+import {task1 } from './todo.js';
 
-functionOne();
+
+const listContainer = document.querySelector('.list-container');
+
+listContainer.textContent = '';
+for(let key in task1){
+    const div = document.createElement('div');
+    div.classList.add('list');
+    div.textContent = task1[key];
+    listContainer.appendChild(div);
+}
