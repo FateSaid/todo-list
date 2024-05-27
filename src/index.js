a1098ce5bcbf01ff9ba2860a7e163241c15c8fba
Fatesaid <fatesaid56@gmail.com>
2024-05-27 20:30:55 +0200

    add to-do list css

--- src/index.js
+++ src/index.js
@@ -5,9 +5,26 @@ import {task1 } from './todo.js';
 const listContainer = document.querySelector('.list-container');
 
 
-for(let key in task1){
-    const div = document.createElement('div');
-    div.classList.add('list');
-    div.textContent = task1[key];
-    listContainer.appendChild(div);
+function listToDom(){
+    const listBox = document.createElement('div');
+    listBox.classList.add('list-box');
+
+    const listTitle = document.createElement('div');
+    listTitle.classList.add('list')
+    const listDescription = document.createElement('div');
+    listDescription.classList.add('hidden');
+    const listDueDate = document.createElement('div');
+    listDueDate.classList.add('list');
+    const listPriority = document.createElement('div');
+    listPriority.classList.add('hidden');
+    listTitle.textContent = task1.title;
+    listDueDate.textContent = task1.dueDate;
+
+    listBox.appendChild(listTitle);
+    listBox.appendChild(listDueDate);
+
+    listContainer.appendChild(listBox);
+    
 }
+
+listToDom();
