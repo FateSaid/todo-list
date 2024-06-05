2214b4ec4026e7ba96c329de8f3285c9a9aad741
Fatesaid <fatesaid56@gmail.com>
2024-06-05 18:06:18 +0200

    create template todo list

--- src/todo.js
+++ src/todo.js
@@ -1,139 +1,65 @@
-
-
-
-function listToDom(title, description, dueDate, priority){
-    const listContainer = document.querySelector('.list-container');
-
-    const listBox = document.createElement('div');
-    listBox.classList.add('list-box');
-    
-    const listContent = document.createElement('div');
-    listContent.classList.add('list-content');
-
-    const listTitle = document.createElement('div');
-    listContent.appendChild(listTitle);
-
-    const listDescription = document.createElement('div');
-
-    const listDueDate = document.createElement('div');
-    listContent.appendChild(listDueDate);
-
-    const listPriority = document.createElement('div');
-
-    listTitle.textContent = title;
-    listDueDate.textContent = dueDate;
-
-    listBox.appendChild(listContent);
-
-    listContainer.appendChild(listBox);
-
-    //hidden content
-    const hiddenContent = document.createElement('div');
-    hiddenContent.classList.add('hidden');
-
-    listDescription.textContent = description;
-    listPriority.textContent = priority;
-    hiddenContent.appendChild(listDescription);
-    hiddenContent.appendChild(listPriority);
-    listBox.appendChild(hiddenContent);
-    
-    //button for list
-
-    listBox.addEventListener('click', () => {
-        hiddenContent.classList.toggle('show');
-    });
-
-    //delete button
-
-   const delBtn = document.createElement('button');
-   delBtn.classList.add('delete-button');
-   delBtn.textContent = 'Delete';
-   listContent.appendChild(delBtn);
-   delBtn.addEventListener('click', (e)=>{
-    e.preventDefault();
-    listBox.remove();
-   });
-
-
-    
+function List(title, description, dueDate, priority){
+    return {title, description, dueDate, priority}
 }
 
-
-
-
-
-
-// add to-do list button
-
-function addBtn(){
-    const addListBtn = document.querySelector('.plus');
+function createList(){
     const dialog = document.querySelector('dialog');
-    addListBtn.addEventListener('click', ()=>{
+    const form = document.querySelector('form');
+    const addBtn = document.querySelector('.add');
+
+    const mainContain = document.querySelector('.main-content');
+
+    const submitBtn = document.querySelector('button[type="submit"]');
+    const cancelBtn = document.querySelector('button[type="cancel"]');
+
+    function newList(){
+        const newL = document.createElement('div');
+        newL.classList.add('list');
+
+        const text = document.getElementById('title');
+        newL.textContent = text.value;
+        mainContain.appendChild(newL);
+    }
+
+    addBtn.addEventListener('click', ()=>{
         dialog.showModal();
     });
-}
 
-//dialog submit button
-
-const submitBtn = document.querySelector('button[type="submit"]');
-
-function clearForm(){
-    const form = document.querySelector('form');
-    form.reset();
-}
-
-//dialog
-
-function submitAction(){
-    const dialog = document.querySelector('dialog');
-
-    const titleDialog = document.getElementById('title').value;
-    const descriptionDialog = document.getElementById('description').value;
-    const dueDateDialog = document.getElementById('dueDate').value;
-    const priorityDialog = document.getElementById('priority').value;
-    listToDom(titleDialog, descriptionDialog, dueDateDialog, priorityDialog);
-    clearForm();
-    dialog.close();
-}
-
-// dialog cancel
-
-const cancelBtn = document.getElementById('cancelBtn');
-
-
-// new project button
-
-function newProject(){
-    const newProjectBtn = document.querySelector('.new-project');
-    newProjectBtn.addEventListener('click', (e)=> {
+    cancelBtn.addEventListener('click', (e)=>{
         e.preventDefault();
-    
-        const projectList = document.createElement('div');
-        projectList.classList.add('project-list');
-    
-    
-        const project = document.querySelector('.project');
-    
-        const captureText = document.getElementById('projectN');
-    
-        projectList.textContent = captureText.value;
+        dialog.close();
+    });
 
-        const delProjectBtn = document.createElement('button');
-        delProjectBtn.classList.add('delete-project-button');
-        delProjectBtn.textContent = 'Delete';
-        projectList.appendChild(delProjectBtn);
-        delProjectBtn.addEventListener('click', ()=>{
-            projectList.remove();
-        })
-        
-        project.appendChild(projectList);
+    submitBtn.addEventListener('click', (e)=>{
+        e.preventDefault();
+        newList();
+        form.reset();
+        dialog.close();
+    });
+}
 
-        clearForm();
-        
-    } );
+function createProject(){
+    const form = document.querySelector('.form-project');
+    const addProjectBtn = document.querySelector('.add-project');
+
+    const sidebarContent = document.querySelector('.sidebar');
+
+
+    const projectText = document.getElementById('project');
+
+    function newProject(){
+        const newProj = document.createElement('div');
+        newProj.classList.add('project-list');
+        newProj.textContent = projectText.value;
+        sidebarContent.appendChild(newProj);
+    }
+
+    addProjectBtn.addEventListener('click', (e)=>{
+        e.preventDefault();
+       newProject();
+       form.reset();
+    });
     
 }
 
-
-
-export {listToDom, addBtn, submitBtn,cancelBtn, clearForm, newProject, submitAction};
+export{createList, createProject}
