47ff5079b37d6c09115316014017bd7147498883
Fatesaid <fatesaid56@gmail.com>
2024-06-06 12:58:26 +0200

    feat: add delete button

--- src/todo.js
+++ src/todo.js
@@ -25,7 +25,6 @@ function setInDom(listTitle, listDescription, listDueDate, listPriority){
         
     });
 
-    console.log(toDoList);
     
     for(let i = 0; i < arr.length; i++){
         const div = document.createElement('div');
